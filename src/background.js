1194af75b4b7dca1cddc28309929630f07fa5186
wesinator <5124946+wesinator@users.noreply.github.com>
2022-06-29 23:15:54 -0400

    remove unused code leftover from template

--- src/background.js
+++ src/background.js
@@ -1,5 +1,3 @@
-// adapted from https://github.com/mdn/webextensions-examples/blob/ec731d4ceee1df0ccdebd87ab9f95875f55eeee7/menu-demo/background.js
-
 /*
 Called when the item has been created, or when creation failed due to an error.
 We'll just log success/failure here.
@@ -19,15 +17,6 @@ We'll just log the error here.
 function onError(error) {
   console.log(`Error: ${error}`);
 }
-
-/*
-Create all the context menu items.
-*/
-/*browser.menus.create({
-  id: "log-selection",
-  title: browser.i18n.getMessage("menuItemSelectionLogger"),
-  contexts: ["selection"]
-}, onCreated);*/
 
 browser.menus.create({
   id: "MD5",
@@ -60,19 +49,6 @@ browser.menus.create({
 }, onCreated);
 
 
-/*browser.menus.create({
-  id: "open-sidebar",
-  title: browser.i18n.getMessage("menuItemOpenSidebar"),
-  contexts: ["all"],
-  command: "_execute_sidebar_action"
-}, onCreated);*/
-
-browser.menus.create({
-  id: "tools-menu",
-  title: browser.i18n.getMessage("menuItemToolsMenu"),
-  contexts: ["tools_menu"],
-}, onCreated);
-
 /*
 The click event listener, where we perform the appropriate action given the
 ID of the menu item that was clicked.
@@ -83,10 +59,6 @@ browser.menus.onClicked.addListener((info, tab) => {
   //console.log(info.selectionText);
 
   switch (info.menuItemId) {
-    case "log-selection":
-      console.log(info.selectionText);
-      break;
-
     case "MD5":
       hash = md5(info.selectionText);
       break;
@@ -98,13 +70,6 @@ browser.menus.onClicked.addListener((info, tab) => {
       break;
     case "SHA512":
       hash = sha512(info.selectionText);
-      break;
-
-    case "open-sidebar":
-      console.log("Opening my sidebar");
-      break;
-    case "tools-menu":
-      console.log("Clicked the tools menu item");
       break;
   }
 
